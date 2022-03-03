a822289ed47d5bf6ca13fb8d11260452fa439559
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-03-03 13:24:47 -0500

    fixed data

--- db/index.js
+++ db/index.js
@@ -35,92 +35,43 @@ const syncAndSeed = async () => {
     await sequelize.sync({ force: true });
     const engineering = await Department.create({ name: "Engineering" });
     const science = await Department.create({ name: "Science" });
-    const danceAndTheather = await Department.create({ name: "Dance & Theather" });
+    const danceAndTheather = await Department.create({
+      name: "Dance & Theather",
+    });
     const literatureAndLanguages = await Department.create({
       name: "Literature & Languages",
     });
     console.log("***********success***********");
 
-    const [
-      computerScience,
-      mechanicalEng,
-      electricalEng,
-      environmentalEng,
-      chemicalEng,
-      computerEng,
-      chemistry,
-      biology,
-      math,
-      arabic,
-      french,
-      english,
-      ballet,
-      theather,
-      modernDance,
-    ] = await Promise.all(
+    await Promise.all(
       [
-        "Computer Science",
-        "Mechanical Engineering",
-        "Electrical Engineering",
-        "Environmental Enginneering",
-        "Chemical Engineering",
-        "Computer Engineering",
-        "Chemistry",
-        "Biology",
-        "Mathematics",
-        "Arabic",
-        "French",
-        "English",
-        "Ballet",
-        "Theather",
-        "Modern Dance",
-      ].map((name) => Major.create({ name }))
+        ["Computer Science", science.id],
+        ["Mechanical Engineering", engineering.id],
+        ["Electrical Engineering", engineering.id],
+        ["Environmental Enginneering", engineering.id],
+        ["Chemical Engineering", engineering.id],
+        ["Computer Engineering", engineering.id],
+        ["Chemistry", science.id],
+        ["Biology", science.id],
+        ["Mathematics", science.id],
+        ["Arabic", literatureAndLanguages.id],
+        ["French", literatureAndLanguages.id],
+        ["English", literatureAndLanguages.id],
+        ["Ballet", danceAndTheather.id],
+        ["Theather", danceAndTheather.id],
+        ["Modern Dance", danceAndTheather.id],
+      ].map(([name, departmentId]) => Major.create({ name, departmentId }))
     );
-
-    computerScience.departmentId = science.id;
-    mechanicalEng.departmentId = engineering.id;
-    electricalEng.departmentId = engineering.id;
-    environmentalEng.departmentId = engineering.id;
-    chemicalEng.departmentId = engineering.id;
-    computerEng.departmentId = engineering.id;
-    chemistry.departmentId = science.id;
-    biology.departmentId = science.id;
-    math.departmentId = science.id;
-    arabic.departmentId = literatureAndLanguages.id;
-    french.departmentId = literatureAndLanguages.id;
-    english.departmentId = literatureAndLanguages.id;
-    ballet.departmentId = danceAndTheather.id;
-    theather.departmentId = danceAndTheather.id;
-    modernDance.departmentId = danceAndTheather.id;
-
-    await Promise.all([
-      computerScience.save(),
-      mechanicalEng.save(),
-      electricalEng.save(),
-      environmentalEng.save(),
-      chemicalEng.save(),
-      computerEng.save(),
-      chemistry.save(),
-      biology.save(),
-      math.save(),
-      arabic.save(),
-      french.save(),
-      english.save(),
-      ballet.save(),
-      theather.save(),
-      modernDance.save(),
-    ]);
   } catch (err) {
     console.log(err);
   }
 };
 
-
 module.exports = {
-    sequelize,
-    syncAndSeed,
-    models: {
-        Department,
-        Major
-    }
-}
+  sequelize,
+  syncAndSeed,
+  models: {
+    Department,
+    Major,
+  },
+};
