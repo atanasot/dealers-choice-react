a6409525dd048c7dc26dc03208ce27bb0af4a69d
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-03-02 20:54:58 -0500

    added delete

--- server.js
+++ server.js
@@ -7,32 +7,37 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.delete("/api/majors/:id", async (req, res, next) => {
+  try {
+    const major = await Major.findByPk(req.params.id);
+    await major.destroy();
+    res.sendStatus(204); //No content
+  } catch (err) {
+    next(err);
+  }
+});
+
 app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-
 app.get("/api/majors", async (req, res, next) => {
   try {
-    const majors = await Major.findAll({
-      include: [Department],
-    });
+    const majors = await Major.findAll();
     res.send(majors);
   } catch (err) {
     next(err);
   }
 });
 
-app.get('/api/departments', async(req, res, next) => {
-    try{
-        const departments = await Department.findAll({
-            include: [Major]
-        })
-        res.send(departments)
-    } catch (err) {
-        next(err)
-    }
-})
+app.get("/api/departments", async (req, res, next) => {
+  try {
+    const departments = await Department.findAll();
+    res.send(departments);
+  } catch (err) {
+    next(err);
+  }
+});
 
 const start = async () => {
   try {
